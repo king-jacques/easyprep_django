bc2a762026a73dd1824211188ded2abe9ccb59b7
jacques <jamestha3d@gmail.com>
2024-09-28 23:32:37 -0400

    allowed hosts

--- prep_tools/settings.py
+++ prep_tools/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = getenv('DEBUG') == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = getenv('ALLOWED_HOSTS').split(",")
 
 
 # Application definition
