42388dc1dd0d7f7a23bbdb441b7848c2ca314f5b
jacques <jamestha3d@gmail.com>
2024-09-29 00:42:17 -0400

    removed aws static file

--- prep_tools/settings.py
+++ prep_tools/settings.py
@@ -183,8 +183,8 @@ AWS_STORAGE_BUCKET_NAME=config('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_REGION_NAME = config('AWS_S3_REGION_NAME', default='us-east-1')  # Change to your region
 
 # s3 boto + django_storage
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = None
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_STORAGE='storages.backends.s3boto3.S3Boto3Storage'
+# AWS_S3_FILE_OVERWRITE = False
+# AWS_DEFAULT_ACL = None
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# STATICFILES_STORAGE='storages.backends.s3boto3.S3Boto3Storage'
 
